873fead022cd2d62fc50162a5637e6b45c8f1f0b
xufeige <yefei-yuefei@163.com>
2016-10-26 12:01:42 +0800

    conflict fixed

--- test.php
+++ test.php
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 Git is a distributed version control system.
 Git is free software distributed under the GPL.
 Git has a mutable index called stage.
-Git tracks changes of files.
-=======
-<?php
-echo 'Just a test';
-echo 'Hello World!';
->>>>>>> ebe60cdd1aeef0ab51b6a3d8f762e605c58e16e8
